2218aed6e99bca040afe84412c7626ce63bd0488
Дарья <144462208+Kuzovnikova@users.noreply.github.com>
2025-11-12 14:49:22 +0500

    Убрала лишние задания

--- functions.h
+++ functions.h
@@ -12,11 +12,6 @@ int sumLustNums(int x);
 int charToNum(char x);
 bool isPositive(int x);
 bool is2Digits(int x);
-bool isUpperCase(char x);
-bool isInRange(int a, int b, int num);
-bool isDivisor(int a, int b);
-bool isEqual(int a, int b, int c);
-int lastNumSum(int a, int b);
 
 ///////////////TASK 2///////////////
 int myAbs(int x);
@@ -24,11 +19,6 @@ double safeDiv(int x, int y);
 bool is35(int x);
 string makeDecision(int x, int y);
 int max3(int x, int y, int z);
-bool sum3(int x, int y, int z);
-int sum2(int x, int y);
-string age(int x);
-string day(int x);
-void printDays(string x);
 
 ///////////////TASK 3///////////////
 string listNums(int x);
@@ -36,8 +26,4 @@ string reverseListNums(int x);
 string chet(int x);
 int pow(int x, int y);
 int numLen(long x);
-bool equalNum(int x);
-void square(int x);
-void leftTriangle(int x);
-void rightTriangle(int x);
-void guessGame();
+
